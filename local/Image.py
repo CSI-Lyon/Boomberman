b8818151c2872c3b921acec9d322622e720e5339
oppZ <mateusz.csi@gmail.com>
2019-05-01 14:04:55 +0200

    Revert "update (#2)"
    This reverts commit a9457348dcaa82f4305e44892abd8bdebc834cc2.

--- local/Image.py
+++
@@ -1,6 +0,0 @@
-from tkinter import PhotoImage
-
-class Image(PhotoImage):
-
-    def __init__(self, directory, name):
-        PhotoImage.__init__(self, file="images/{}/{}.png".format(directory, name))
